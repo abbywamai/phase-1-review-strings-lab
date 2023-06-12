4074c275e963c7cce480c2c1763b6520885a8352
abigail <abigail.wamai@student.moringaschool.com>
2023-06-12 12:10:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = `Muthoni Wamai`;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, Muthoni Wamai!`;
+
+const shortGreeting = `Welcome, M!`;
